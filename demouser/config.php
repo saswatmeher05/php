c1cd3591e7305291660d4c857faa67e9ab39dd57
Saswat Meher <64583159+saswatmeher05@users.noreply.github.com>
2021-04-06 18:57:12 +0530

    DemoUser_Updated

--- demouser/config.php
+++ demouser/config.php
@@ -1,4 +1,5 @@
 <?php
+
     session_start();
     $dbHost="localhost";
     $dbUsername="root";
--- demouser/delete backup.php
+++ demouser/delete backup.php
@@ -1,13 +1,8 @@
 <?php 
 include 'config.php';
 $id=$_GET['id'];
-if($_SESSION["login_id"]==1){
 $deletequery="delete users,education from users inner join education on education.user_id=users.id where users.id=$id";
 $query=mysqli_query($dbc,$deletequery);
 
 header("Location:dashboard.php");
-}
-else{
-    header("Location:dashboard_common.php");
-}
 ?>
